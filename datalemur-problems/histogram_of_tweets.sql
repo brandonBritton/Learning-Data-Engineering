ff11ddc31cb5c23452473acafe122ee0beb25f1d
Brandon <brandonbritton.dev@gmail.com>
2026-09-14 13:55:16 +1000

    clearer CTE name, ISO date format, explicit 2022 end date, removal of unnecessary DISTINCT, and deterministic ordering.

--- datalemur-problems/histogram_of_tweets.sql
+++ datalemur-problems/histogram_of_tweets.sql
@@ -6,19 +6,19 @@
   It counts the number of users who have tweeted a certain number of times and groups them into buckets.
 */
 
+WITH tweets_per_user AS (
+  SELECT
+    user_id,
+    COUNT(tweet_id) AS tweet_count
+  FROM tweets
+  WHERE tweet_date >= '2022-01-01'
+  AND tweet_date < '2023-01-01'
+  GROUP BY user_id
+)
+
 SELECT
-  DISTINCT tweets_num AS tweet_bucket,
+  tweet_count AS tweet_bucket,
   COUNT(user_id) AS users_num
-FROM (
-  SELECT 
-    user_id, 
-    COUNT(tweet_id) AS tweets_num
-  FROM 
-    tweets
-  WHERE 
-    tweet_date >= '01/01/2022'
-  GROUP BY 
-    user_id
-) AS total_tweets
-GROUP BY 
-  tweets_num;
+FROM tweets_per_user
+GROUP BY tweet_count    
+ORDER BY tweet_bucket;
